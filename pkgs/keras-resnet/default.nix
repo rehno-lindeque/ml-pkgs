afaff7d8070b451a66aa32172e3bc4e9446b6bb9
Rehno Lindeque <errantkid@gmail.com>
2018-05-02 21:27:58 -0400

    init keras-resnet at 7160dd5

---
+++ pkgs/keras-resnet/default.nix
@@ -0,0 +1,25 @@
+{ fetchFromGitHub
+, python
+, pythonPackages
+}:
+
+pythonPackages.buildPythonPackage rec {
+  version = "dev";
+  name = "keras-retinanet-${version}";
+  src = fetchFromGitHub {
+    owner = "broadinstitute";
+    repo = "keras-resnet";
+    rev = "7160dd5cf8f97b2bb70a72e6f1eba5ad867cd1a1";
+    sha256 = "0qyzbyyy7lv0g1lbhllc71gs0n09if9b34hvdsh2y9ynfzd9l8sw";
+  };
+  propagatedBuildInputs =
+    (with pythonPackages; [
+      tensorflow
+      Keras
+    ])
+    ++ [
+      python
+    ];
+  doCheck = false;
+}
+
