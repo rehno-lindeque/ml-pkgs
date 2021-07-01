18be95330f5b37356d208d177f31c1b06452d8f8
Rehno Lindeque <errantkid@gmail.com>
2021-07-01 13:14:00 -0400

    pytorch-bin: init at 1.7.1

---
+++ pkgs/pytorch/binary-hashes.nix
@@ -0,0 +1,10 @@
+version: {
+  # x86_64-linux-37 = {
+  #   url = "https://download.pytorch.org/whl/cu102/torch-${version}-cp37-cp37m-linux_x86_64.whl";
+  #   sha256 = "0000000000000000000000000000000000000000000000000000";
+  # };
+  x86_64-linux-38 = {
+    url = "https://download.pytorch.org/whl/cu102/torch-${version}-cp38-cp38-linux_x86_64.whl";
+    sha256 = "0gnpy01gxnwn3z9m2g49w7r8fcixyv3vpvw61nb3ds4m1j4ccbyx";
+  };
+}
