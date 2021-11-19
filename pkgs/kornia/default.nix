b5f8820a7b93d5dea111a5ff4c2f0e06b9c993bd
Rehno Lindeque <errantkid@gmail.com>
2021-11-18 23:06:01 -0500

    kornia: init at 0.6.1

---
+++ pkgs/kornia/default.nix
@@ -0,0 +1,42 @@
+{ buildPythonPackage
+, fetchPypi
+, lib
+, packaging
+, pytest
+, pytest-runner
+, pytorch
+}:
+
+buildPythonPackage rec {
+  pname = "kornia";
+  version = "0.6.1";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1mnwbwlhsv2f1mbxacbiizyqbm5n23sn5hb6b1a6d1pq14rznf7n";
+  };
+
+  buildInputs = [
+    pytest-runner
+  ];
+
+  checkInputs = [
+    pytest
+  ];
+
+  propagatedBuildInputs = [
+    packaging
+    pytorch
+  ];
+
+  doCheck = false; # test fails for unknown reason atm
+
+  pythonImportsCheck = [ "kornia" ];
+
+  meta = with lib; {
+    description = "Kornia is a differentiable library that allows classical computer vision to be integrated into deep learning models.";
+    homepage = "https://kornia.readthedocs.io/";
+    license = licenses.asl20;
+    maintainers = with maintainers; [];
+  };
+}
---
+++ pkgs/kornia/sandbox.nix
@@ -0,0 +1,50 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+
+let
+  python = pkgs.python38.override {
+    packageOverrides = python-self: python-super: {
+      kornia = python-self.callPackage ./. {};
+    };
+  };
+  pythonEnv = python.withPackages (ps:
+    with ps;
+    [
+      kornia
+    ]
+  );
+in
+pkgs.mkShell {
+  buildInputs = [
+    pythonEnv
+  ];
+  shellHook =
+    let
+      nc="\\e[0m"; # No Color
+      white="\\e[1;37m";
+      black="\\e[0;30m";
+      blue="\\e[0;34m";
+      light_blue="\\e[1;34m";
+      green="\\e[0;32m";
+      light_green="\\e[1;32m";
+      cyan="\\e[0;36m";
+      light_cyan="\\e[1;36m";
+      red="\\e[0;31m";
+      light_red="\\e[1;31m";
+      purple="\\e[0;35m";
+      light_purple="\\e[1;35m";
+      brown="\\e[0;33m";
+      yellow="\\e[1;33m";
+      grey="\\e[0;30m";
+      light_grey="\\e[0;37m";
+    in
+      ''
+      echo ""
+      printf "${white}"
+      echo "-----------------------------------------------"
+      echo "kornia sandbox environment"
+      echo "-----------------------------------------------"
+      printf "${nc}"
+      echo
+      '';
+}
