7b479c3853b5ec841bc45acda9a5590b88f32f38
Rehno Lindeque <errantkid@gmail.com>
2022-08-16 12:55:01 -0400

    Maintain imported nix files via the flake inputs (supported as of NixOS/nix#6548)

--- flake.nix
+++ flake.nix
@@ -4,6 +4,44 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    # Nix imports
+    amazon-s3-plugin-for-pytorch = {
+      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/835b9f2b58277fac67eb96b5ab2d21f2315295e6/pkgs/development/python-modules/amazon-s3-plugin-for-pytorch/default.nix";
+      flake = false;
+    };
+    pytorch-unstable = {
+      url = "https://raw.githubusercontent.com/rehno-lindeque/nixpkgs/f43cfcd81387c453ef74da55e098cd076885a2b2/pkgs/development/python-modules/pytorch/unstable.nix";
+      flake = false;
+    };
+    torchdata-unstable = {
+      url = "https://raw.githubusercontent.com/rehno-lindeque/nixpkgs/7c1748a0e607bbfc24be50d13c6b54f671be5af5/pkgs/development/python-modules/torchdata/unstable.nix";
+      flake = false;
+    };
+    expecttest = {
+      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/da2b767621fd538881e130d12ecdfdd3195727a3/pkgs/development/python-modules/expecttest/default.nix";
+      flake = false;
+    };
+    sacrebleu = {
+      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/64ab7ab9debb6d93311083a735419f837d8d6022/pkgs/development/python-modules/sacrebleu/default.nix";
+      flake = false;
+    };
+    fairseq = {
+      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/3b4512affed042a5bab8debdd6a41b0eb7d143da/pkgs/development/python-modules/fairseq/default.nix";
+      flake = false;
+    };
+    torchtext = {
+      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/c96132b4a0df81106c91bddca6f895fe5d44469e/pkgs/development/python-modules/torchtext/default.nix";
+      flake = false;
+    };
+    torchaudio = {
+      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/ffe409095cfcec679fe0f0a97db6adb4c0c6d32d/pkgs/development/python-modules/torchaudio/default.nix";
+      flake = false;
+    };
+    tensorflow-tensorboard = {
+      url = "https://raw.githubusercontent.com/rehno-lindeque/nixpkgs/e6997a4c433c117563443952e5de5cd4265d0065/pkgs/development/python-modules/tensorflow-tensorboard/default.nix";
+      flake = false;
+    };
   };
 
   outputs = {
--- overlays.nix
+++ overlays.nix
@@ -1,51 +1,5 @@
 { flake }:
 let
-  # Inputs that cannot be specified in the flake inputs schema (e.g. single file urls)
-  additionalInputs = {
-    amazon-s3-plugin-for-pytorch = builtins.fetchurl {
-      url = https://raw.githubusercontent.com/NixOS/nixpkgs/835b9f2b58277fac67eb96b5ab2d21f2315295e6/pkgs/development/python-modules/amazon-s3-plugin-for-pytorch/default.nix;
-      sha256 = "sha256:1v4xs6abb1wlrccb4a8r71l355hg86xhwm8ilc8vly2p15vryxnv";
-    };
-    pytorch-unstable = builtins.fetchurl {
-      url = https://raw.githubusercontent.com/rehno-lindeque/nixpkgs/f43cfcd81387c453ef74da55e098cd076885a2b2/pkgs/development/python-modules/pytorch/unstable.nix;
-      sha256 = "sha256:1fi41id5kmlviykqfrjlvm43dnlkd0155zvcr4aw2x756i2f6z5g";
-    };
-    torchdata-unstable = builtins.fetchurl {
-      url = https://raw.githubusercontent.com/rehno-lindeque/nixpkgs/7c1748a0e607bbfc24be50d13c6b54f671be5af5/pkgs/development/python-modules/torchdata/unstable.nix;
-      sha256 = "sha256:094zsm80wwiivpxnrwmdck30vn2zhjzy99szpjwi18rzv3ll0rfx";
-    };
-    expecttest = builtins.fetchurl {
-      # https://github.com/NixOS/nixpkgs/pull/160197
-      url = https://raw.githubusercontent.com/NixOS/nixpkgs/da2b767621fd538881e130d12ecdfdd3195727a3/pkgs/development/python-modules/expecttest/default.nix;
-      sha256 = "sha256:0hvisfpzy20y3cfk2mhsyqazg4xp5aj147nrxs7yrmilbxb68l0b";
-    };
-    sacrebleu = builtins.fetchurl {
-      # https://github.com/NixOS/nixpkgs/pull/160205
-      url = https://raw.githubusercontent.com/NixOS/nixpkgs/64ab7ab9debb6d93311083a735419f837d8d6022/pkgs/development/python-modules/sacrebleu/default.nix;
-      sha256 = "sha256:1md4fa3xjv1qcy4yqj7gvc5y9zm2zpv6syfb6k0z229xc46589rx";
-    };
-    fairseq = builtins.fetchurl {
-      # https://github.com/NixOS/nixpkgs/pull/160206
-      url = https://raw.githubusercontent.com/NixOS/nixpkgs/3b4512affed042a5bab8debdd6a41b0eb7d143da/pkgs/development/python-modules/fairseq/default.nix;
-      sha256 = "sha256:0hjiq8qrrw6wckbkk4i4zy9gydn660al28sfd0r65iwsjnl68lfp";
-    };
-    torchtext = builtins.fetchurl {
-      # https://github.com/NixOS/nixpkgs/pull/160207
-      url = https://raw.githubusercontent.com/NixOS/nixpkgs/c96132b4a0df81106c91bddca6f895fe5d44469e/pkgs/development/python-modules/torchtext/default.nix;
-      sha256 = "sha256:1x1v9xawi0x9grg39vrj7ia6iqf4kv31bchn2qsam856bf9w5dxq";
-    };
-    torchaudio = builtins.fetchurl {
-      # https://github.com/NixOS/nixpkgs/pull/160210
-      url = https://raw.githubusercontent.com/NixOS/nixpkgs/ffe409095cfcec679fe0f0a97db6adb4c0c6d32d/pkgs/development/python-modules/torchaudio/default.nix;
-      sha256 = "sha256:1i5h5rsx8r1wf4a7m1di0mf7kpsjqwlc8ndi0iayc8mlkd7hrz3v";
-    };
-    tensorflow-tensorboard = builtins.fetchurl {
-      # ...
-      url = https://raw.githubusercontent.com/rehno-lindeque/nixpkgs/e6997a4c433c117563443952e5de5cd4265d0065/pkgs/development/python-modules/tensorflow-tensorboard/default.nix;
-      sha256 = "sha256:1117mrchn1d06308ycjd9ww0zwy5z5pylmw2midsgzkxnpisvkb7";
-    };
-  };
-
   pythonOverrides = final: prev: let
     inherit (final.pkgs) fetchFromGitHub;
 
@@ -89,13 +43,13 @@ let
     };
   in {
     albumentations = final.callPackage ./pkgs/albumentations {};
-    amazon-s3-plugin-for-pytorch = final.callPackage additionalInputs.amazon-s3-plugin-for-pytorch {};
+    amazon-s3-plugin-for-pytorch = final.callPackage flake.inputs.amazon-s3-plugin-for-pytorch {};
     aravis = final.callPackage ./pkgs/aravis {};
     darkflow = final.callPackage ./pkgs/darkflow {};
     efficientnet-pytorch = final.callPackage ./pkgs/efficientnet-pytorch {};
     einops = (final.callPackage ./pkgs/einops {}).overridePythonAttrs override-einops;
-    expecttest = final.callPackage additionalInputs.expecttest {};
-    fairseq = (final.callPackage additionalInputs.fairseq {}).overrideAttrs override-fairseq;
+    expecttest = final.callPackage flake.inputs.expecttest {};
+    fairseq = (final.callPackage flake.inputs.fairseq {}).overrideAttrs override-fairseq;
     ideepcolor = final.callPackage ./pkgs/ideepcolor {};
     imagenet-utils = final.callPackage ./pkgs/imagenet-utils {};
     keras = final.callPackage ./pkgs/keras {};
@@ -107,26 +61,26 @@ let
     pretrainedmodels = final.callPackage ./pkgs/pretrainedmodels {};
     # pytorch-bin = final.callPackage ./pkgs/pytorch/bin.nix {};
     pytorch-nightly-bin = final.callPackage ./pkgs/pytorch/nightly-bin.nix { };
-    pytorch-unstable = final.callPackage additionalInputs.pytorch-unstable {
+    pytorch-unstable = final.callPackage flake.inputs.pytorch-unstable {
       cudaSupport = true;
       # Using the previous pytorch is necessary because pytorch-unstable is defined in terms of pytorch.
       # That is, this avoids a cycle if the next overlay defines pytorch = pytorch-unstable.
       inherit (prev) pytorch;
     };
     qdarkstyle = final.callPackage ./pkgs/qdarkstyle {};
-    sacrebleu = final.callPackage additionalInputs.sacrebleu {};
+    sacrebleu = final.callPackage flake.inputs.sacrebleu {};
     segmentation-models-pytorch = final.callPackage ./pkgs/segmentation-models-pytorch {};
-    tensorflow-tensorboard = final.callPackage additionalInputs.tensorflow-tensorboard {};
+    tensorflow-tensorboard = final.callPackage flake.inputs.tensorflow-tensorboard {};
     timm = final.callPackage ./pkgs/timm {};
-    torchaudio = (final.callPackage additionalInputs.torchaudio {}).overridePythonAttrs override-torchaudio;
+    torchaudio = (final.callPackage flake.inputs.torchaudio {}).overridePythonAttrs override-torchaudio;
     torchaudio-nightly-bin = final.callPackage ./pkgs/torchaudio/nightly-bin.nix { pytorch = final.pytorch-nightly-bin; };
     torchdata-nightly-bin = final.callPackage ./pkgs/torchdata/nightly-bin.nix { pytorch = final.pytorch-nightly-bin; };
-    torchdata-unstable = (final.callPackage additionalInputs.torchdata-unstable {
+    torchdata-unstable = (final.callPackage flake.inputs.torchdata-unstable {
       pytorch = final.pytorch-nightly-bin;
       torchaudio = final.torchaudio-nightly-bin;
       torchtext = final.torchtext-nightly-bin;
     }).overridePythonAttrs override-torchdata;
-    torchtext = (final.callPackage additionalInputs.torchtext {}).overrideAttrs override-torchtext;
+    torchtext = (final.callPackage flake.inputs.torchtext {}).overrideAttrs override-torchtext;
     torchtext-nightly-bin = final.callPackage ./pkgs/torchtext/nightly-bin.nix { pytorch = final.pytorch-nightly-bin; };
     torchvision-nightly-bin = final.callPackage ./pkgs/torchvision/nightly-bin.nix { pytorch = final.pytorch-nightly-bin; };
     wandb = final.callPackage ./pkgs/wandb {};
